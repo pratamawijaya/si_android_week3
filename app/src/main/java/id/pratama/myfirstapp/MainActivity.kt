5a64e0d71e7b0d04eaf1a43a7c0decec0cdb6f51
pratamawijaya <set.mnemonix@gmail.com>
2023-08-28 20:05:13 +0700

    first commit

---
+++ app/src/main/java/id/pratama/myfirstapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package id.pratama.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// git config --global user.name "pratamawijaya"
+// git config --global user.email "set.mnemonix@gmail.com"
