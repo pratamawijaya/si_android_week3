375f84cbc1f7127b7407703bbe4e5184fab33db4
pratamawijaya <set.mnemonix@gmail.com>
2023-08-28 20:35:42 +0700

    update project

--- app/src/main/java/id/pratama/myfirstapp/MainActivity.kt
+++ app/src/main/java/id/pratama/myfirstapp/MainActivity.kt
@@ -2,11 +2,19 @@ package id.pratama.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myButton: Button = findViewById(R.id.myButton)
+
+        myButton.setOnClickListener {
+            val result = 10000 / 10
+        }
+
     }
 }
 
